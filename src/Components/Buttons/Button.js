3c5825fbdf1c84efe7220f96e9e721d9dace58cc
lineberner <line437x@stud.kea.dk>
2022-05-27 14:07:26 +0200

    back button

--- src/Components/Buttons/Button.js
+++ src/Components/Buttons/Button.js
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Button() {
+	const navigate = useNavigate();
 	return (
-		<button className="back">
-			<a href="javascript:history.back()">← Go Back</a>
+		<button className="back" onClick={() => navigate(-1)}>
+			← Go Back
 		</button>
 	);
 }
