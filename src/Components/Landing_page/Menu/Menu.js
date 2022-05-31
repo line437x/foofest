a049c960aab3eadd92eabf86fdf59353a3247f8b
lineberner <line437x@stud.kea.dk>
2022-05-31 12:47:12 +0200

    chance in style on menu

--- src/Components/Landing_page/Menu/Menu.js
+++ src/Components/Landing_page/Menu/Menu.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
-import pic1 from "../../../img/place_1.jpg";
+import pic1 from "../../../img/place_2.jpg";
 import pic2 from "../../../img/place_2.jpg";
 import pic3 from "../../../img/place_3.jpg";
 
 export default function Navigation() {
 	return (
-		<div className="navigation">
+		<section className="navigation">
 			<nav>
 				<ul className="grid">
 					<Link className="link" to="/schedule">
 						<li>
-							<img src={pic1} alt="billede" />
+							{/* <figure>
+								<div className="menu_img"></div>
+							</figure> */}
+							{/* <img src={pic1} alt="billede" /> */}
 							<h3>SCHEDULE</h3>
 						</li>
 					</Link>
@@ -22,15 +25,23 @@ export default function Navigation() {
 					<Link className="link" to="/festivalmap">
 						<li>
 							<h3>FESTIVAL MAP</h3>
-							<img src={pic2} alt="billede" />
+							{/* <figure>
+								<div className="menu_img"></div>
+							</figure> */}
+							{/* <img src={pic2} alt="billede" /> */}
 						</li>
 					</Link>
-					<li>
-						<img src={pic3} alt="billede" />
-						<h3>GET YOUR TICKET HERE</h3>
-					</li>
+					<a href="https://foofest-booking.netlify.app" target="_blank">
+						<li>
+							{/* <figure>
+							<div className="menu_img"></div>
+						</figure> */}
+							{/* <img src={pic3} alt="billede" /> */}
+							<h3>GET YOUR TICKET HERE</h3>
+						</li>
+					</a>
 				</ul>
 			</nav>
-		</div>
+		</section>
 	);
 }
