bff4a4b990dbe8ec92b132e99911ec00e53f7d80
lineberner <line437x@stud.kea.dk>
2022-06-02 17:15:13 +0200

    style on playing now (desktop)

--- src/Components/Playing_now/Playing_now.js
+++ src/Components/Playing_now/Playing_now.js
@@ -101,27 +101,26 @@ export default function PlayingNow() {
 		<div id="playing_now">
 			<Banner banner="PLAYING NOW"></Banner>
 			<Navigation setScene={setScene}></Navigation>
-			<Button></Button>
 			<div className="content_container">
-				<article>
-					<h2>{scene}</h2>
-					<p>
-						Browse the stages and we will make sure that you are always updated on now and whos next
-						up
-					</p>
-				</article>
-				<div className="content">
-					<div>
-						<h2>
-							Now playing at <span>{scene}</span>
-						</h2>
-						<Current act={playingNow.act} start={playingNow.start} end={playingNow.end}></Current>
+				<section>
+					<Button></Button>
+					<article>
+						<h2>{scene}</h2>
+						<p>Browse the stages and we will make sure that you are always updated on now and whos next up.</p>
+					</article>
+					<div className="content">
+						<div>
+							<h2>
+								Now playing at <span>{scene}</span>
+							</h2>
+							<Current act={playingNow.act} start={playingNow.start} end={playingNow.end}></Current>
+						</div>
+						<div>
+							<h2>Coming up next</h2>
+							<Next playingNext={playingNext}></Next>
+						</div>
 					</div>
-					<div>
-						<h2>Coming up next</h2>
-						<Next playingNext={playingNext}></Next>
-					</div>
-				</div>
+				</section>
 				<Sidebar></Sidebar>
 			</div>
 		</div>
